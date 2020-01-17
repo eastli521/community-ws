96674b84e6f47059f5fa25400c66fade6711f382
wangshuai191226 <wangshuai191226@credithc.com>
2020-01-17 11:41:50 +0800

    模拟登录流程

--- src/main/java/life/majiang/community1/Controller/AuthorizeController.java
+++ src/main/java/life/majiang/community1/Controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package life.majiang.community1.Controller;
 
 import life.majiang.community1.dto.AccessTokenDTO;
+import life.majiang.community1.dto.GithubUser;
 import life.majiang.community1.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,16 @@ public class AuthorizeController {
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        githubProvider.getAccessToken(accessTokenDTO);
+        //这是一个模拟登录的过程，实际上，我们在第一次访问github时除了code,其他参数传过去了（index.html)
+        //code应该是github为了确认时返回的code（返回页面在GitHub中定义了）
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("de84b852c893ffa9cec0");
+        accessTokenDTO.setClient_secret("37d6043057f1fa12a2f5a481fe6ee08f4ec3b380");
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        String access_token = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(access_token);
+        System.out.println(githubUser.getName());
         return "Index";
     }
 }
--- src/main/java/life/majiang/community1/provider/GithubProvider.java
+++ src/main/java/life/majiang/community1/provider/GithubProvider.java
@@ -1,6 +1,7 @@
 package life.majiang.community1.provider;
 
 import life.majiang.community1.dto.AccessTokenDTO;
+import life.majiang.community1.dto.GithubUser;
 import okhttp3.*;
 import com.alibaba.fastjson.*;
 import org.springframework.stereotype.Component;
@@ -9,18 +10,45 @@ import java.io.IOException;
 
 @Component
 public class GithubProvider {
+    /**
+     *这里的getAccessTokenDTO模拟的是再次访问GitHub确认登陆时的过程，这时github给我们返回access_token,赋予登录身份
+     * @param accessTokenDTO
+     * @return String access_token
+     */
     public String getAccessToken(AccessTokenDTO accessTokenDTO) {
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        JSONObject json = new JSONObject();
-        RequestBody body = RequestBody.create(String.valueOf(json),JSON);
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO),mediaType);
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
         try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            String string = response.body().string(); //string:access_token=15e311ef9386a3e26df3cf2268c17bce9f851ea4&scope=user&token_type=bearer
+            String[] split = string.split("&");
+            String access_token = split[0].split("=")[1];
+            return access_token;
         }catch(IOException e){
+        }
+        return null;
+    }
+
+    /**
+     * 这里是模拟带着access_token访问GitHub获取user身份的过程
+     * @param access_token
+     * @return GithubUser githubUser
+     */
+    public GithubUser getUser(String access_token){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+access_token)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (IOException e){
         }
         return null;
     }
